a2cca8e2c53175b843e35d012e092656094943fc
Irene R <irene@smallwondermedia.com>
2018-10-01 23:52:26 -0400

    view, add new, update in manager

--- bamazonManager.js
+++ bamazonManager.js
@@ -12,48 +12,84 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
 if (err) throw err;
 console.log("connected as id " + connection.threadId);
-managerOptions();
-});
+addNewProduct();
+})
 
-function managerOptions() {
-    connection.query("SELECT*FROM products", 
-    function(err, res) {
-        if (err) throw err;
+// function managerOptions() {
+//     connection.query("SELECT*FROM products", 
+//     function(err, res) {
+//         if (err) throw err;
 
-        inquirer.prompt([
-            // manager's options
-            {
-            name: "managerOptions",
-            type: "list",
-            message: "What do you want to do?",
-            choices: ["[View Products for Sale]", "[View Low Inventory]", "[Add to Inventory]", "[Add New Product]"]
-            }
-            .then(function(answer) {
-                console.log(answer);
-                if (answer.managerOptions === "View Products for Sale") {
-                    viewProducts();
-                }
-                if (answer.managerOptions === "Add New Product") {
-                    addNewProduct();
-                }
-            })
-        ])
-    })
-};
+//         inquirer.prompt([
+//             // manager's options
+//             {
+//             name: "managerOptions",
+//             type: "list",
+//             message: ["[VIEW] Products for Sale", "View [LOW] Inventory", "[ADD] to Inventory", "Add [NEW] Product"],
+//             choices: ["VIEW", "LOW", "ADD", "NEW"]
+//             }
+//             .then(function(answer) {
+//                 console.log(answer);
+//                 if (answer.managerOptions.toUpperCase() === "VIEW") {
+//                     viewProducts();
+//                 }
+//                 if (answer.managerOptions.toUpperCase() === "LOW") {
+//                     addNewProduct();
+//                 }
+//             })
+//         ])
+//     })
+// };
 // end managerOptions();
 
+// viewProducts();
 function viewProducts() {
     connection.query("SELECT*FROM products",
-    function(err, res) {
-        if (err) throw err;
-        console.log(res);
-    })
+        function(err, res) {
+            if (err) throw err;
+            console.log(res);
+        });
 };
 // end viewProducts();
 
 
 
 function addNewProduct() {
-
+    console.log("Insert new product");
+    connection.query("INSERT INTO products SET ?",
+        {
+            product_name: "Unicorns",
+            department_name: "Mythical Creatures",
+            price: 10000.00,
+            stock_quantity: 1
+        },
+        function(err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " Product Added!");
+            updateProducts();
+        }
+    );
 };
-// end addNewProduct();
+// // end addNewProduct();
+
+function updateProducts() {
+    connection.query("UPDATE products SET ? WHERE ?",
+    [
+        {product_name: "Unicorns"},
+        {department_name: "Mythical Creatures"},
+        {price: 1000000.00},
+        {stock_quantity: 1}
+    ],
+    function(err, res) {
+        connection.query("SELECT product_name FROM products",
+        function(err, res){
+            console.log(res)
+        });
+    }
+    )
+};
+// end updateProduct();
+
+
+
+// connection.end();
